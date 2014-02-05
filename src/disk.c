351b7f74ebb583b6d79f654a355707c8dfd8621f
Patrick Mooney <patrick.f.mooney@gmail.com>
2014-02-05 11:45:37 -0600

    Fix autotools and update coding style

--- src/disk.c
+++ src/disk.c
@@ -290,20 +290,21 @@ static counter_t disk_calc_time_incr (counter_t delta_time, counter_t delta_ops)
  * Otherwise it returns *disk_name
  */
 
-char* disk_udev_attr_name (struct udev *udev, char *disk_name, const char *attr)
+static char *disk_udev_attr_name (struct udev *udev, char *disk_name, const char *attr)
 {
 	struct udev_device *dev;
 	const char *prop;
 	char *output = NULL;
 
-	dev = udev_device_new_from_subsystem_sysname(udev, "block", disk_name);
+	dev = udev_device_new_from_subsystem_sysname (udev, "block", disk_name);
 	if (dev != NULL)
 	{
-		prop = udev_device_get_property_value(dev, attr);
+		prop = udev_device_get_property_value (dev, attr);
 		if (prop) {
-			output = strdup(prop);
+			output = strdup (prop);
+			DEBUG ("disk plugin: renaming %s => %s", disk_name, output);
 		}
-		udev_device_unref(dev);
+		udev_device_unref (dev);
 	}
 	if (output == NULL)
 	{
